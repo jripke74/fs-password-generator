1adc0685c34b7a74cec94ce721c5157b96a89083
Jeff Ripke <jripke74@gmail.com>
2025-06-25 09:50:24 -0700

    remove comments

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Build a Password Generator App
-// In this lab, you'll practice using functions by building
-// a random password generator.
-
-// Objective: Fulfill the user stories below and get all the
-// tests to pass to complete the lab.
-
-// User Stories:
-
-// 1. You should create a function called generatePassword
-// that takes a parameter, indicating the length of generated
-// password. You can name the parameter whatever you like.
-// 2. Your function should return a string which represents a
-// randomly generated password. You should use the following
-// string and different Math methods to help you return a new
-// string with random characters in
-// it: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*().
-
 function generatePassword(passLength) {
   let value = "";
   let characterString =
